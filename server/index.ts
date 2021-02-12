397a84c6ebf758c04ab4e0f87c3110dbaa009b6a
Alexandru Constantin <alexandru.constantin@brokergenius.com>
2021-02-12 03:05:15 +0100

    Fix

--- server/index.ts
+++ server/index.ts
@@ -164,9 +164,9 @@ io.on('connection', socket => {
 try {
     const tail = new Tail(tetraKitLogPath);
     tail.on('error', (error) => console.error(error));
-    tail.on('line', (line) => {
-        line = JSON.parse(line);
-        if (line.service === 'CMCE'
+    tail.on('line', (line:string) => {
+        const parsedLine = JSON.parse(line);
+        if (parsedLine.service === 'CMCE'
         ) {
             io.emit('cmceLog', line);
         }
